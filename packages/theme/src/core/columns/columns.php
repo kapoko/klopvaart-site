472a8ed10092a001f3e78a482b8b75d670d2399c
kapoko <17455539+kapoko@users.noreply.github.com>
2024-10-09 09:51:44 +0200

    Extend core columns with custom restricted first column option

---
+++ packages/theme/src/core/columns/columns.php
@@ -0,0 +1,33 @@
+<?php
+
+add_action(
+    'init',
+    function () {
+        wp_enqueue_block_style(
+            'core/columns',
+            [
+                'handle' => 'klopvaart/core-colums/styles',
+                'src'    => get_stylesheet_directory_uri() . '/build/style-core.css',
+                'ver'    => wp_get_theme()->get('Version'),
+                'path'   => get_stylesheet_directory() . '/build/style-core.css',
+            ]
+        );
+    }
+);
+
+add_filter('render_block_core/columns', function ($block_content, $block) {
+    if (! isset($block['attrs']['restrictedFirstColumn'])) {
+        return $block_content;
+    }
+
+    $restrictedFirstColumn = $block['attrs']['restrictedFirstColumn'];
+
+    $p = new WP_HTML_Tag_Processor($block_content);
+    if ($p->next_tag()) {
+        $p->add_class('has-restricted-first-column');
+    }
+
+    $block_content = $p->get_updated_html();
+
+    return $block_content;
+}, 10, 2);
