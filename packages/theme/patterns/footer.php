050fcbac01bae14917c4db5cfe3067451b67dda1
kapoko <17455539+kapoko@users.noreply.github.com>
2024-11-06 17:55:18 +0100

    Add temp footer

---
+++ packages/theme/patterns/footer.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Title: Footer
+ * Slug: klopvaart/footer
+ * Categories: footer
+ */
+?>
+
+<!-- wp:group {"metadata":{"categories":["footer"],"patternName":"core/fullwidth-footer-with-background-color-and-three-columns","name":"Fullwidth footer with background color and three columns"},"align":"full","className":"has-background-color","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"spacing":{"padding":{"top":"var:preset|spacing|60","right":"var:preset|spacing|40","bottom":"var:preset|spacing|60","left":"var:preset|spacing|40"},"margin":{"top":"0","bottom":"0"},"blockGap":"var:preset|spacing|30"},"dimensions":{"minHeight":"40vh"}},"backgroundColor":"black","textColor":"white","layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch","verticalAlignment":"center"}} -->
+<div class="wp-block-group alignfull has-background-color has-white-color has-black-background-color has-text-color has-background has-link-color" style="min-height:40vh;margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--40)"><!-- wp:columns {"align":"wide","fontSize":"small"} -->
+<div class="wp-block-columns alignwide has-small-font-size"><!-- wp:column {"layout":{"type":"constrained"}} -->
+<div class="wp-block-column"><!-- wp:navigation {"ref":28,"overlayMenu":"never","style":{"spacing":{"blockGap":"8px"}},"fontSize":"large","layout":{"type":"flex","orientation":"vertical","justifyContent":"left","flexWrap":"nowrap"}} /-->
+
+<!-- wp:spacer {"height":"24px"} -->
+<div style="height:24px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
