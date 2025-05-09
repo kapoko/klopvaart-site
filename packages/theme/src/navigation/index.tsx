69b0738e016c25468cca9d76c4b21156ae9448fe
kapoko <17455539+kapoko@users.noreply.github.com>
2025-05-09 18:49:14 +0200

    wip

--- packages/theme/src/navigation/index.tsx
+++ packages/theme/src/navigation/index.tsx
@@ -1,9 +1,9 @@
 import { registerBlockType } from "@wordpress/blocks";
 import type { Template } from "@wordpress/blocks";
 import {
-  InnerBlocks,
-  useBlockProps,
-  useInnerBlocksProps,
+    InnerBlocks,
+    useBlockProps,
+    useInnerBlocksProps,
 } from "@wordpress/block-editor";
 
 import "./style.scss";
@@ -11,53 +11,53 @@ import "./style.scss";
 import metadata from "./block.json";
 
 const TEMPLATE: Template[] = [
-  [
-    "core/navigation",
-    {
-      overlayMenu: "never",
-    },
-  ],
+    [
+        "core/navigation",
+        {
+            overlayMenu: "never",
+        },
+    ],
 ];
 
 const MenuButton = ({ className }: { className: string }) => {
-  return (
-    <button className={`hamburger hamburger--chop ${className}`} type="button">
-      <div className="inner">
-        <span className="bar" />
-        <span className="bar" />
-        <span className="bar" />
-      </div>
-    </button>
-  );
+    return (
+        <button className={`hamburger hamburger--chop ${className}`} type="button">
+            <div className="inner">
+                <span className="bar" />
+                <span className="bar" />
+                <span className="bar" />
+            </div>
+        </button>
+    );
 };
 
 registerBlockType(metadata.name, {
-  ...metadata,
-  icon: "admin-site-alt2",
-  attributes: {},
-  edit: () => {
-    return (
-      <div
-        {...useBlockProps({
-          className:
-            "max-w-full md:container mx-auto px-4 py-2 md:py-10 justify-end md:justify-start flex items-center",
-        })}
-      >
-        <div className="hidden md:block">
-          <InnerBlocks
-            allowedBlocks={metadata.allowedBlocks}
-            templateLock={"contentOnly"}
-            template={TEMPLATE}
-          />
-        </div>
-        <MenuButton className={"md:hidden!"} />
-      </div>
-    );
-  },
-  save: () => {
-    const blockProps = useBlockProps.save();
-    const innerBlocksProps = useInnerBlocksProps.save(blockProps);
+    ...metadata,
+    icon: "admin-site-alt2",
+    attributes: {},
+    edit: () => {
+        return (
+            <div
+                {...useBlockProps({
+                    className:
+                        "max-w-full md:container mx-auto px-4 py-2 md:py-10 justify-end md:justify-start flex items-center",
+                })}
+            >
+                <div className="hidden md:block">
+                    <InnerBlocks
+                        allowedBlocks={metadata.allowedBlocks}
+                        templateLock={"contentOnly"}
+                        template={TEMPLATE}
+                    />
+                </div>
+                <MenuButton className={"md:hidden!"} />
+            </div>
+        );
+    },
+    save: () => {
+        const blockProps = useBlockProps.save();
+        const innerBlocksProps = useInnerBlocksProps.save(blockProps);
 
-    return <div {...innerBlocksProps} />;
-  },
+        return <div {...innerBlocksProps} />;
+    },
 });
