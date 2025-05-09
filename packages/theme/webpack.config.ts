f986f70c350c67ccfa99149908577be4e6557703
kapoko <17455539+kapoko@users.noreply.github.com>
2025-05-09 16:57:54 +0200

    Remove usage of __dirname from webpack config

--- packages/theme/webpack.config.ts
+++ packages/theme/webpack.config.ts
@@ -1,19 +1,15 @@
 import path from "node:path";
-import { fileURLToPath } from "node:url";
 
 import defaultConfig from "@wordpress/scripts/config/webpack.config.js";
 const [scriptConfig, moduleConfig] = defaultConfig;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const config = {
-  ...scriptConfig,
-  entry: {
-    ...scriptConfig.entry(),
-    core: path.resolve(__dirname, "src/core/index.ts"),
-    global: path.resolve(__dirname, "src/global.ts"),
-  },
+    ...scriptConfig,
+    entry: {
+        ...scriptConfig.entry(),
+        core: path.resolve("./src/core/index.ts"),
+        global: path.resolve("./src/global.ts"),
+    },
 };
 
 export default [config, moduleConfig];
