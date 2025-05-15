b5939520db2f2b13a7cb7f36bfa350930ec1c040
kapoko <17455539+kapoko@users.noreply.github.com>
2025-05-15 23:18:30 +0200

    Add favicon

--- packages/theme/functions.php
+++ packages/theme/functions.php
@@ -158,6 +158,13 @@ add_action('wp_head', function () {
         <meta property="og:type" content="website">
         <meta property="og:url" content="{$url}">
         <meta property="og:image" content="{$image}">
+
+        <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="Klopvaart" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
     HTML;
 
     echo $meta . "\n";
