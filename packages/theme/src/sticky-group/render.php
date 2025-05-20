887181a118bb1cd845cecf56cc73f08154b56031
kapoko <17455539+kapoko@users.noreply.github.com>
2025-05-20 16:55:28 +0200

    List margin, sticky group better responsiveness

--- packages/theme/src/sticky-group/render.php
+++ packages/theme/src/sticky-group/render.php
@@ -2,6 +2,6 @@
 /** @var $content */
 ?>
 
-<div <?php echo get_block_wrapper_attributes(["class" => "sticky top-[10svh] xl:top-[20svh] z-1"]); ?>>
+<div <?php echo get_block_wrapper_attributes(["class" => "sticky top-[10svh] lg:top-[15vh] xl:top-[20svh] z-1"]); ?>>
     <?php echo $content ?>
 </div>
