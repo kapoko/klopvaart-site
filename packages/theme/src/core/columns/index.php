73c27e2b41cad1efe700a6006a26a9e988d3dea0
kapoko <17455539+kapoko@users.noreply.github.com>
2025-05-20 21:36:33 +0200

    Updated dependencies, minor styling changes

--- packages/theme/src/core/columns/index.php
+++ packages/theme/src/core/columns/index.php
@@ -16,3 +16,15 @@ add_filter('render_block_core/columns', function ($block_content, $block) {
 
     return $block_content;
 }, 10, 2);
+
+
+add_filter('render_block_core/columns', function ($block_content, $block) {
+    $p = new WP_HTML_Tag_Processor($block_content);
+    if ($p->next_tag()) {
+        $p->add_class('!mb-4 !gap-x-[1rem] lg:!gap-x-[2rem] xl:!gap-x-[3rem]');
+    }
+
+    $block_content = $p->get_updated_html();
+
+    return $block_content;
+}, 10, 2);
