8ae95a473dd463dba2d58f57bab7114d3f8dfbb7
kapoko <17455539+kapoko@users.noreply.github.com>
2025-12-17 13:45:32 +0100

    Update dependencies, change webpack config to js for compatibility with node ^24

--- packages/theme/webpack.config.js
+++ packages/theme/webpack.config.js
@@ -1,15 +1,15 @@
 import path from "node:path";
-
 import defaultConfig from "@wordpress/scripts/config/webpack.config.js";
+
 const [scriptConfig, moduleConfig] = defaultConfig;
 
 const config = {
-    ...scriptConfig,
-    entry: {
-        ...scriptConfig.entry(),
-        core: path.resolve("./src/core/index.ts"),
-        global: path.resolve("./src/global.ts"),
-    },
+  ...scriptConfig,
+  entry: {
+    ...scriptConfig.entry(),
+    core: path.resolve("./src/core/index.ts"),
+    global: path.resolve("./src/global.ts"),
+  },
 };
 
 export default [config, moduleConfig];
